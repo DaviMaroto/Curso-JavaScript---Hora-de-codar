57a11d63d7733a4895563c22c3ca47fdba5a35a8
DaviMaroto <davimaroto09@gmail.com>
2022-04-02 23:40:03 -0300

    Segundo exercicio em javascript

---
+++ Exercicio_02/script.js
@@ -0,0 +1,7 @@
+//Escreva três valores em Number em um arquivo e exiba no navegador com console.log()
+
+//Com números inteiros,números com casa decimal e por aritmética 
+
+console.log(3)
+console.log(10.5)
+console.log (4 * (3 + 10) / 2 * (5))
