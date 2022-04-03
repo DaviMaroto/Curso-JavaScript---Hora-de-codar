dd6aab29fbbe6c12aad422eecf4bb34db841a697
DaviMaroto <davimaroto09@gmail.com>
2022-04-02 23:36:37 -0300

    Primeiro exercicio em Javascript

---
+++ Exercicio_01/script.js
@@ -0,0 +1,7 @@
+//Escreva três valores em Strings em um arquivo e exiba no navegador com o console.log
+
+//Com aspas duplas simples e template literals
+
+console.log("Meu nome é Davi")
+console.log('O nome dele é João')
+console.log(`a soma de 4 por 2 é ${4 + 2}`)
